eea8f2a8ad058ce3c227906565d5ebf017f47811
Владислав Кузнецов <kz-vlad@mail.ru>
2021-03-09 19:56:32 +0300

    Добавьте файлы проекта.

---
+++ homework4_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter numbers: ");
+                string inputString = Console.ReadLine();
+                string[] numberStrings = inputString.Split(new char[] { ' ' });
+
+                int sum = 0;
+                foreach (string tempString in numberStrings)
+                {
+                    int tempNumber = 0;
+                    if (Int32.TryParse(tempString, out tempNumber) == true)
+                    {
+                        sum += tempNumber;
+                    }
+                }
+
+                Console.WriteLine("The sum is " + sum);
+                Console.ReadKey();
+            }
+        }
+    }
+}
